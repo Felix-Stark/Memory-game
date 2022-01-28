fe550c03b1630e423cbb5fc57859d06c10d97514
Felix Stark <felix.b.stark@gmail.com>
2022-01-28 22:09:44 +0100

    memory-game

--- JS/index-memory.js
+++ JS/index-memory.js
@@ -5,33 +5,34 @@ const overlayElem = document.querySelector('.overlay');
 const closeElem = document.querySelector('close');
 
 
+
 let flipCounter = 0;
+let pairCounter = '';
 let cardsToCompare = [];
 let firstDraw = cardsToCompare[0];
 let secondDraw = cardsToCompare[1];
 
 
+// eventlistener 'click'. sends card to function flip(card)
 for(i = 0; i < cardsElem.length; i++) {
-   let pickedCard = cardsElem[i];
-   pickedCard.addEventListener('click', ()=> {flip(pickedCard)});
-   cardsToCompare.push(pickedCard);
-}
-
-function flip(pick) {
-   pick.classList.toggle("flip");
-   flipCounter++;
-   console.log(flipCounter)
-   if(flipCounter == 2) {
-      ()=> {compareCards(cardsToCompare)}
-      
-   }
-}
-
-function compareCards() {
+   let card = cardsElem[i];
    
-
-   for(i = 0; j < cardsToCompare.length; j++)
-   if(cardsToCompare[0] == cardsToCompare[1]) {
-      cardsToCompare.slice()
-   }
+   card.addEventListener('click', ()=> {theGame(card)});
 }
+// , cardsToCompare.push(card.attributes[1].value)
+
+// apply class="flip" on clicked card. send flipped cards value to function compareCards
+function theGame(card) {
+   card.classList.toggle("flip");
+   flipCounter++;
+   
+   if(flipCounter > 0 && flipCounter < 3) {
+      cardsToCompare.push(card.attributes[1].value);
+   }
+   if(cardsToCompare)
+   
+   console.log(cardsToCompare)
+   console.log('antal flips: ', flipCounter);
+}
+
+
