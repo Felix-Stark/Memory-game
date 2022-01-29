888e8e0c3627d8aa50e6ae97a72663023471ecca
Felix Stark <felix.b.stark@gmail.com>
2022-01-29 20:17:55 +0100

    update memory-card

--- JS/index-memory.js
+++ JS/index-memory.js
@@ -3,35 +3,44 @@ const cardsParent = document.querySelector('.memory-cards');
 const frontElem = document.querySelectorAll('.front');
 const backElem = document.querySelectorAll('.back');
 const overlayElem = document.querySelector('.overlay');
-const closeElem = document.querySelector('close');
-
-
+const closeElem = document.querySelector('.close');
+const shuffleButton = document.querySelector('.shuffle-btn');
 
 let flipCounter = 0;
 let pairCounter = '';
+let shuffleCounter = '';
 let cardsToCompare = [];
 let firstDraw = cardsToCompare[0];
 let secondDraw = cardsToCompare[1];
 
-let shuffleButton = document.querySelector('.shuffle-btn').addEventListener('click', ()=> {shuffleCards(shuffle)});
 
-function shuffleCards() {
-   //let cardArr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
- 
-   for (let i = cardsParent.children.length; i >= 0; i--) {
-      cardsParent.appendChild(cardsParent.children[Math.random() * i | 0]);
-   }
+shuffleButton.addEventListener('click', () => {shuffleCards()});
+
+closeElem.addEventListener('click', ()=> {closeOverlay()});
+   
+function closeOverlay() {
+   overlayElem.classList.toggle('show');
 }
 
+function shuffleCards(shuffled) {
+   for (i = cardsParent.children.length; i >= 0; i--) {
+      let randomCards = cardsParent.appendChild(cardsParent.children[Math.random() * i | 0]);
+      
+      console.log(randomCards)
+      shuffleCounter++;
+   }
+return shuffled;
+}
 
 // eventlistener 'click'. sends card to function flip(card)
 for(i = 0; i < cardsElem.length; i++) {
    let card = cardsElem[i];
-   
+
    card.addEventListener('click', ()=> {theGame(card)});
 }
 
 function theGame(card) {
+
    card.classList.toggle("flip");
    flipCounter++;
    
@@ -41,16 +50,19 @@ function theGame(card) {
 
    if(flipCounter == 2){
       if(cardsToCompare[0] == cardsToCompare[1]){
-
+         pairCounter++;
          let cardValue = card.attributes[1].value;
 
          let cardElement = document.querySelectorAll(`[data-card="${cardValue}"]`);
 
-         for(i = 0; i < cardElement.length; i++) {
-            let element = cardElement[i];
+         setTimeout(function() {
 
-            element.classList.toggle('hide');
-         }
+            for(i = 0; i < cardElement.length; i++) {
+               let element = cardElement[i];
+   
+               element.classList.toggle('hide');
+            }
+         },1000);
 
          flipCounter = 0;
          cardsToCompare = [];
@@ -71,8 +83,12 @@ function theGame(card) {
          cardsToCompare = [];
       }
    }
-   
+   if (pairCounter == 8) {
+      overlayElem.classList.toggle('show');
+   }
+
    console.log(cardsToCompare)
    console.log('antal flips: ', flipCounter);
+   console.log('antal par: ', pairCounter)
+   
 }
-
